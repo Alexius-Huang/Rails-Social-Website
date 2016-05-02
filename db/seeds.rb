5da44528318ac28c9f3a33dce1deee0d7de19304
Maxwell-Alexius <destinydragon0614@yahoo.com.tw>
2016-05-02 17:53:55 +0800

    User basic profile setup complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Seed file processing..."
+
+print "Should generate topics..."
+
+gewnerate_topics = ["Life Experience", "Programming Discussion", "Ruby Lang.", "Ruby on Rails", "Web Development"].each { |topic| Topic.create(title: topic) }
+
+puts "Complete!"
+
+print "Should generate articles..."
+
+generate_articles = (1..10).each do |i|
+	Article.create(
+		title: "Article number : #{i}",
+		content: "This is the content of the article #{i}",
+		topic_id: rand(1..3)
+	)
+end
+
+puts "Complete!"
