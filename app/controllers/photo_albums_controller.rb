8e59848b3c889c300c21a9103017397b414ecec8
Maxwell-Alexius <destinydragon0614@yahoo.com.tw>
2016-05-04 17:18:55 +0800

    Set up pinterest style

--- app/controllers/photo_albums_controller.rb
+++ app/controllers/photo_albums_controller.rb
@@ -3,13 +3,14 @@ class PhotoAlbumsController < ApplicationController
   	@photo_albums = PhotoAlbum.all
   	@new_photo_album = PhotoAlbum.new
   	@photo_highlight_token = "All Photos"
+  	@photos = Photo.order("created_at DESC").paginate(page: params[:page], per_page: 10)
   end
 
   def show
   	@photo_albums = PhotoAlbum.all
   	@photo_album = PhotoAlbum.find(params[:id])
   	@new_photo_album = PhotoAlbum.new
-  	@photos = @photo_album.photos.all
+  	@photos = @photo_album.photos.order("created_at DESC").paginate(page: params[:page], per_page: 10)
   	@photo_highlight_token = @photo_album.title
   end
 
