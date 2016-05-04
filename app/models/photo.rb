7f5ad3554805732dc3f6f3ec71f460b4e2b120c3
Maxwell-Alexius <destinydragon0614@yahoo.com.tw>
2016-05-04 15:49:16 +0800

    User main page photo presence complete

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
 	validates :title, :content, :image, presence: true
-	belongs_to :photo_albums
+	belongs_to :photo_album
 	mount_uploader :image, ImageUploader
 end
