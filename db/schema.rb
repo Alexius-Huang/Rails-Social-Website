41fb5d01bd67eba5ba3ccb29b9282ef05eaaac09
Maxwell-Alexius <destinydragon0614@yahoo.com.tw>
2016-05-08 15:06:51 +0800

    Registration pages css styled complete; Except edit_registration.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507065357) do
+ActiveRecord::Schema.define(version: 20160508063128) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20160507065357) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
